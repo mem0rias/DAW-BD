63c3b4df945d982bb77b657dde895464f8ec70c8
mem0rias <79435043+mem0rias@users.noreply.github.com>
2022-08-22 17:06:13 -0500

    Intro a JS

---
+++ Lab5/intro.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+console.info("Esto es importante");
+console.warn("Esto es una advertencia");
+console.error(1===1);
+
+// Comparar valores
+console.assert(1 == true);
+    
+//=== compara valor y tipo de dato
+    console.assert(1 == true);
+console.assert("1" == 1 );
+const precio = 9.99999;
+var Robot = "Fanuc"
+for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+let carrera = "ISDR";
+console.log(carrera);
+for(let j = 0; j < 10; j++){
+    console.log(j);
+}
+
+console.log(1+3+"4");
+console.log("4"+3+2+1);
