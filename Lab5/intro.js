4c1b9bf0df3e4c65b1b1ddd59817471e1eeb0942
mem0rias <79435043+mem0rias@users.noreply.github.com>
2022-08-22 23:52:27 -0500

    Terminados labs

--- Lab5/intro.js
+++ Lab5/intro.js
@@ -22,3 +22,34 @@ for(let j = 0; j < 10; j++){
 
 console.log(1+3+"4");
 console.log("4"+3+2+1);
+
+const nombre = prompt("como te llamas?");
+console.log("hola" + nombre);
+const respuesta = confirm("Tienes sueño?");
+if(respuesta){
+    console.log("Necesitas cafe");
+}else{
+    console.log("No necestias cafe");
+}
+
+function tomar_cafe(){
+    console.log("Glu glu glu");
+}
+
+tomar_cafe();
+function tomar(bebida){
+    console.log("Tomando " + bebida);
+}
+tomar("te");
+//Funciones modernas
+const comer = (comiendo) => {
+    console.log("Comiendo" + comiendo);
+}
+
+comer("Pollo");
+
+let comiendo  = comer;
+comiendo(" Panditas");
+
+// Arregla
+const arreglo = [1,2,3,"Elemento"]
