9c5e139c0bdbfc639456991870841ea5f2458fb3
mem0rias <79435043+mem0rias@users.noreply.github.com>
2022-09-19 17:03:36 -0500

    Modulo de Express Terminado

--- Express/app.js
+++ Express/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}));
+
+const robots = [];
 //Middleware
 
 app.use((request, response, next) => {
@@ -28,6 +32,43 @@ app.use('/roots/root', (request, response, next) => {
 app.use('/roots', (request, response, next) => {
     response.send('Casi root pa, pero esto no es linux');
     next();
+});
+
+app.get('/robots/new', (request, response, next) => {
+  
+});
+
+app.post('/robots/new', (request, response, next) => {
+    console.log(request.body);
+    robots.push(request.body.nombre);
+    response.redirect('/robots');
+});
+
+app.use('/robots/new', (request, response, next) => {
+    let html = '<!DOCTYPE html>';
+    html += "<h1>Registrar robot</h1>";
+    html += '<form action="/robots/new" method="POST">';
+    html += '<label for="nombre">Nombre del robot: </label>';
+    html += '<input type="text" id="nombre" name="nombre">';
+    html += "<br><br>";
+    html += '<input type="submit" id="enviar" name="enviar" value="Registrar">';
+    html += "</form>";
+    response.send(html); 
+});
+
+app.use('/robots', (request, response, next) => {
+    let html = '<!DOCTYPE html>';
+    html += "<h1>Estos son los mejores robots</h1>";
+    html += "<ul>";
+    for (let r of robots) {
+        html += "<li>" + r +"</li>";
+    }
+    html += "</ul>";
+    response.send(html); 
+});
+
+app.use((request, response, next) => {
+    response.status(404).send('<h1>Error 404</h1>'); //Manda la respuesta
 });
 
 app.listen(3000);
